8c862a08753114929b5c91a8953a5c3192f6cd86
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2024-08-19 23:25:34 +0200

    feat: setup and first draft of select university component

---
+++ react-select-university/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SelectUniversity from "./components/selectUniversity/SelectUniversity";
+
+function App() {
+  return (
+    <>
+      <SelectUniversity />
+    </>
+  );
+}
+
+export default App;
---
+++ react-select-university/src/components/selectUniversity/SelectUniversity.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { University } from "../../types";
+
+function SelectUniversity() {
+  const [query, setQuery] = useState("");
+
+  console.log({ query });
+
+  async function fetchUniversities(): Promise<University[]> {
+    const response = await fetch(
+      `http://universities.hipolabs.com/search?name=${query}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  }
+
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["universities"],
+    queryFn: fetchUniversities,
+    enabled: query.length > 3,
+  });
+
+  console.log({ data });
+
+  return (
+    <>
+      <div>SelectUniversity</div>
+      <input
+        type="text"
+        placeholder="Search University..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error fetching data</div>}
+
+      {data && (
+        <select>
+          {data.map((university: University) => (
+            <option key={university.name} value={university.name}>
+              {university.name}
+            </option>
+          ))}
+        </select>
+      )}
+    </>
+  );
+}
+
+export default SelectUniversity;
