9a91971c9756e745550247358ce30d2e0f26caf1
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2024-08-20 21:55:40 +0200

    style: import style file into component

--- react-select-university/src/components/selectUniversity/SelectUniversity.tsx
+++ react-select-university/src/components/selectUniversity/SelectUniversity.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { University } from "../../types";
+import "./SelectUniversity.css";
 
 function SelectUniversity() {
   const [query, setQuery] = useState("");
