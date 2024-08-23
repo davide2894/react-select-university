ee78fe4aab7828811d6a41eb15a1202877e80bbe
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2024-08-23 17:29:15 +0200

    fix: remove uncomplete function that caused build errors

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,15 +1,7 @@
 import { render } from '@testing-library/react';
-import React, { ReactNode } from 'react';
+import React from 'react';
 
 import { QueryClient } from "@tanstack/react-query";
-
-
-declare module "react-query/types/react/QueryClientProvider" {
-  interface QueryClientProviderProps {
-    children?: ReactNode;
-  }
-}
-
 
 export function renderWithClient(client: QueryClient, ui: React.ReactElement) {
   const { rerender, ...result } = render(
